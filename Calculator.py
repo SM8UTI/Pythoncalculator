96beca73589d26eb463f2e24957a8e085528f69f
HackerTub <72093296+HackerTub@users.noreply.github.com>
2020-09-30 19:41:17 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -3,41 +3,42 @@ author      : Smruti Ranjan Nayak
 Github Link : https://github.com/SmrutiRN
 """
 sym = "*"
-print(sym * 40)
+print(sym * 45)
 x = int(input("[1] First Number  : "))
 y = int(input("[2] Second Number : "))
-print(sym * 40)
+print(sym * 45)
 z = """
     [+] Add 
     [-] Subtracts
-    [*] Multiplies
     [/] Divides
+    [*] Multiplies
+    
 
 """
 print (z)
 action = str(input("Enter your Choice(EX : + , - ): "))
-print(sym * 40)
+print(sym * 45)
 
 if action == "+":
     add = int(x + y)
     print("[+] Your Value : ",add)
-    print(sym * 40)
+    print(sym * 45)
 elif action == "-":
     substract = int(x - y)
     print("[-] Your Value :",substract)
-    print(sym * 40)
+    print(sym * 45
 elif action == "*":
     multiplies = int(x * y)
     print("[*] Your Value :",multiplies)
-    print(sym * 40)
+    print(sym * 45
 elif action == "/":
     divide = int(x / y)
     print("[/] Your Value :",divide)
-    print(sym * 40)
+    print(sym * 45
 else:
 
     print("Plz choice wright option ? ")
-    print(sym * 40)
+    print(sym * 45
 
 
 
