a0c37fb8289c1fc3826a4913c0033c572963dfed
Fytex <pedropauloqt@gmail.com>
2020-10-01 03:17:01 +0100

    Cleaner code

--- Calculator.py
+++ Calculator.py
@@ -9,6 +9,7 @@ def click(event):
     global scvalue
     text = event.widget.cget("text")
     print(text)
+    value = ""
     if text=="=":
         if scvalue.get().isdigit():
             value = int(scvalue.get())
@@ -18,15 +19,12 @@ def click(event):
             except Exception as e:
                 print(e)
                 value = "Error"
-        scvalue.set(value)
-        screen.update()
 
-    elif text=="c":
-        scvalue.set("")
-        screen.update()
-    else:
-        scvalue.set(scvalue.get()+ text)
-        screen.update()
+    elif text!="c":
+        value = scvalue.get()+ text
+
+    scvalue.set(value)
+    screen.update()
 
 root = Tk()
 root.geometry("600x900")
@@ -38,85 +36,44 @@ scvalue.set("")
 screen = Entry(root, textvar=scvalue, font="lucida 30 bold")
 screen.pack(fill=X, ipadx=8, pady=10, padx=10)
 
+
+for i in range(9 ,0, -3):
+    f = Frame(root, bg="grey")
+    
+    for i in range(i, i-3, -1):
+        b = Button(f, text=str(i), padx=10, pady=5, font="lucida 35 bold")
+        b.pack(side=LEFT, padx=10, pady=5)
+        b.bind("<Button-1>", click)
+        
+    f.pack()
+
+
 f = Frame(root, bg="grey")
-b = Button(f, text="9", padx=10, pady=5, font="lucida 35 bold")
-b.pack(side=LEFT, padx=10, pady=5)
-b.bind("<Button-1>", click)
 
-b = Button(f, text="8", padx=10, pady=5, font="lucida 35 bold")
-b.pack(side=LEFT, padx=10, pady=5)
-b.bind("<Button-1>", click)
+for char in "0-*":
+    b = Button(f, text=char, padx=10, pady=5, font="lucida 35 bold")
+    b.pack(side=LEFT, padx=13, pady=5)
+    b.bind("<Button-1>", click)
 
-b = Button(f, text="7", padx=10, pady=5, font="lucida 35 bold")
-b.pack(side=LEFT, padx=10, pady=5)
-b.bind("<Button-1>", click)
 f.pack()
 
 f = Frame(root, bg="grey")
-b = Button(f, text="6", padx=10, pady=5, font="lucida 35 bold")
-b.pack(side=LEFT, padx=10, pady=5)
-b.bind("<Button-1>", click)
 
-b = Button(f, text="5", padx=10, pady=5, font="lucida 35 bold")
-b.pack(side=LEFT, padx=10, pady=5)
-b.bind("<Button-1>", click)
+for char in "/%=":
+    b = Button(f, text=char, padx=10, pady=5, font="lucida 35 bold")
+    b.pack(side=LEFT, padx=9, pady=5)
+    b.bind("<Button-1>", click)
 
-b = Button(f, text="4", padx=10, pady=5, font="lucida 35 bold")
-b.pack(side=LEFT, padx=10, pady=5)
-b.bind("<Button-1>", click)
-f.pack()
-
-
-f = Frame(root, bg="grey")
-b = Button(f, text="3", padx=10, pady=5, font="lucida 35 bold")
-b.pack(side=LEFT, padx=10, pady=5)
-b.bind("<Button-1>", click)
-
-b = Button(f, text="2", padx=10, pady=5, font="lucida 35 bold")
-b.pack(side=LEFT, padx=10, pady=5)
-b.bind("<Button-1>", click)
-
-b = Button(f, text="1", padx=10, pady=5, font="lucida 35 bold")
-b.pack(side=LEFT, padx=10, pady=5)
-b.bind("<Button-1>", click)
 f.pack()
 
 f = Frame(root, bg="grey")
-b = Button(f, text="0", padx=10, pady=5, font="lucida 35 bold")
-b.pack(side=LEFT, padx=13, pady=5)
-b.bind("<Button-1>", click)
 
-b = Button(f, text="-", padx=10, pady=5, font="lucida 35 bold")
-b.pack(side=LEFT, padx=13, pady=5)
-b.bind("<Button-1>", click)
-
-b = Button(f, text="*", padx=10, pady=5, font="lucida 35 bold")
-b.pack(side=LEFT, padx=13, pady=5)
-b.bind("<Button-1>", click)
-f.pack()
-
-f = Frame(root, bg="grey")
-b = Button(f, text="/", padx=10, pady=5, font="lucida 35 bold")
-b.pack(side=LEFT, padx=9, pady=5)
-b.bind("<Button-1>", click)
-
-b = Button(f, text="%", padx=10, pady=5, font="lucida 35 bold")
-b.pack(side=LEFT, padx=9, pady=5)
-b.bind("<Button-1>", click)
-
-b = Button(f, text="=", padx=10, pady=5, font="lucida 35 bold")
-b.pack(side=LEFT, padx=9, pady=5)
-b.bind("<Button-1>", click)
-f.pack()
-
-f = Frame(root, bg="grey")
 b = Button(f, text="c", padx=10, pady=5, font="lucida 35 bold")
 b.pack(side=LEFT, padx=5, pady=5)
 b.bind("<Button-1>", click)
 
 b = Button(f, text="clac", padx=10, pady=5, font="lucida 25 bold")
 b.pack(side=LEFT, padx=5, pady=5)
-
 
 
 b = Button(f, text="+", padx=10, pady=5, font="lucida 35 bold")
